99b91c6fa399240f91094c105cbb083340ab66e2
cdjovenr <51064583+cdjovenr@users.noreply.github.com>
2019-07-11 18:58:32 -0500

    Update Parabolico_VerletAnim.py

--- Parabolico_VerletAnim.py
+++ Parabolico_VerletAnim.py
@@ -13,26 +13,24 @@ def verlet (pos_actual,pos_anterior, aceleracion,deltaT):
 
 def euler (pos_actual,vel_actual, aceleracion, deltaT):
 	return pos_actual+vel_actual*deltaT+0.5*aceleracion*deltaT**2
+
+def grafica(pos_actualx,pos_actualy):
+	plt.clf()
+	plt.xlim(0,8)
+	plt.ylim(0,60)
+	plt.plot(pos_actualx,pos_actualy,color="green",marker='o', linestyle='None')
+	plt.pause(0.1)
+
 plt.ion()
 plt.figure()
-plt.xlim(0,8)
-plt.ylim(0,60)
-plt.plot(pos_actualx[-1],pos_actualy[-1],color="green",marker='o', linestyle='None')
-plt.pause(0.1)
+grafica(pos_actualx[-1],pos_actualy[-1])
 pos_actualx.append(euler(pos_actualx,vel[0],0,dt))
 pos_actualy.append(euler(pos_actualy,vel[1],aceleracion,dt))
 
-plt.clf()
-plt.xlim(0,8)
-plt.ylim(0,60)
-plt.plot(pos_actualx[-1],pos_actualy[-1],color="green",marker='o', linestyle='None')
-plt.pause(0.1)
+grafica(pos_actualx[-1],pos_actualy[-1])
 
 while pos_actualy[-1]>=0:
 	pos_actualx.append(verlet(pos_actualx[-1],pos_actualx[-2],0,dt))
 	pos_actualy.append(verlet(pos_actualy[-1],pos_actualy[-2],aceleracion,dt))
 	plt.clf()
-	plt.xlim(0,8)
-	plt.ylim(0,60)
-	plt.plot(pos_actualx[-1],pos_actualy[-1],color="green",marker='o', linestyle='None')
-	plt.pause(0.1)
+	grafica(pos_actualx[-1],pos_actualy[-1])
